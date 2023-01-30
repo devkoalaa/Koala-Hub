24b32358936ea563e051cf7cd2c777a5367b90b9
Herberticleber Carvalho Valadares <herbert.valadares@ssp.df.gov.br>
2023-01-30 17:38:23 -0300

    Adicionado stats da season atual

--- src/app/fortnite/fortnite.component.ts
+++ src/app/fortnite/fortnite.component.ts
@@ -9,36 +9,58 @@ import { FormControl } from '@angular/forms'
 })
 export class FortniteComponent {
 	url = 'https://fortnite-api.com/v2/stats/br/v2/'
+	head: HttpHeaders = new HttpHeaders().append(
+		'Authorization',
+		'3a924cb0-69a8-43d5-aa6d-69fc12b40991'
+	)
 	playerBusca = new FormControl()
-	resultPlayer: any
+	resultAllPlayer: any
+	resultSeasonPlayer: any
 	visible: string = 'hidden'
 	playerName: string = ''
 	playerLvl: string = ''
-	playerWins: string = ''
-	playerKills: string = ''
+	playerAllWins: string = ''
+	playerAllKills: string = ''
+	playerSeasonWins: string = ''
+	playerSeasonKills: string = ''
 	playerImg: string = ''
 
 	constructor(private http: HttpClient) {}
 
 	searchPlayer(playerName: string) {
-		this.getApiPkm(playerName).subscribe((data) => {
+		this.getAllStats(playerName).subscribe((data) => {
 			this.visible = ''
-			this.resultPlayer = data
-			this.playerName = this.resultPlayer.data.account.name
-			this.playerImg = this.resultPlayer.data.image
-			this.playerLvl = this.resultPlayer.data.battlePass.level
-			this.playerWins = this.resultPlayer.data.stats.all.overall.wins
-			this.playerKills = this.resultPlayer.data.stats.all.overall.kills
+			this.resultAllPlayer = data
+			this.playerName = this.resultAllPlayer.data.account.name
+			this.playerLvl = this.resultAllPlayer.data.battlePass.level
+			this.playerAllWins = this.resultAllPlayer.data.stats.all.overall.wins
+			this.playerAllKills = this.resultAllPlayer.data.stats.all.overall.kills
+			// this.playerImg = this.resultAllPlayer.data.image
+			this.playerImg =
+				'https://fortnite-api.com/images/cosmetics/br/glider_id_068_garageband/featured.png'
 
-			console.log('searchPlayer: ', this.resultPlayer)
+			// console.log('searchPlayer: ', this.resultPlayer)
+		})
+
+		this.getSeasonStats(playerName).subscribe((data) => {
+			this.resultSeasonPlayer = data
+			this.playerSeasonWins = this.resultSeasonPlayer.data.stats.all.overall.wins
+			this.playerSeasonKills = this.resultSeasonPlayer.data.stats.all.overall.kills
+
+			// console.log('searchPlayer: ', this.resultSeasonPlayer)
 		})
 	}
 
-	getApiPkm(playerName: string) {
-		const head = new HttpHeaders().append('Authorization', '3a924cb0-69a8-43d5-aa6d-69fc12b40991')
+	getAllStats(playerName: string) {
+		const params = new HttpParams().append('name', playerName)
+		// .append('image', 'all')
 
-		const params = new HttpParams().append('name', playerName).append('image', 'all')
+		return this.http.get(this.url, { headers: this.head, params })
+	}
 
-		return this.http.get(this.url, { headers: head, params })
+	getSeasonStats(playerName: string) {
+		const params = new HttpParams().append('name', playerName).append('timeWindow', 'season')
+
+		return this.http.get(this.url, { headers: this.head, params })
 	}
 }
